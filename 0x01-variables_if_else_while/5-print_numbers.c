96bcb274aebaac803fe6fef26a2a6572389207ab
dominicmwikya <dominicmwikya50@gmail.com>
2023-07-14 15:35:17 +0000

    Update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<sdtio.h>
+/**
+ * main - Print single numbers of base starting from ten.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i=0; i<10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
