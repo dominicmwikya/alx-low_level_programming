88bc291840f337db6bafbef85cf480293766f155
dominicmwikya <dominicmwikya50@gmail.com>
2023-07-14 16:16:18 +0000

    Update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,6 +12,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
         
@@ -27,5 +28,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n,  n % 10);
 	}
+
 	return (0);
 }
