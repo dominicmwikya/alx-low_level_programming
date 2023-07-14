0f05eeb04401aaa3a02ef1c0a17d74aa4de4ad9e
dominicmwikya <dominicmwikya50@gmail.com>
2023-07-14 16:45:32 +0000

    3 print alphat update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-	char alp[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-	for (i=0; i<52; i++)
+
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
