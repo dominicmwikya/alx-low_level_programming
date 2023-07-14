4a2e96bbbdfbb1e821557943fbad40a89bc5b6c5
dominicmwikya <dominicmwikya50@gmail.com>
2023-07-14 16:26:18 +0000

    Update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
+
 /**
- *  * main -Prints the alphabet in lowercase.
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main -Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 { 
-      	char lowerCase[26]="abcdefghijklmnopqrstuvwxyz"; 
-        int i;
-        for (i=0; i<26; i++)
+	char lowerCase[26]="abcdefghijklmnopqrstuvwxyz"; 
+	int i;
+	
+	for (i = 0; i < 26; i++)
         {
    	     putchar(lowerCase[i]);
        	}
-
-         putchar('\n');
-         return (0);
+	putchar('\n');
+       	return (0);
 }
