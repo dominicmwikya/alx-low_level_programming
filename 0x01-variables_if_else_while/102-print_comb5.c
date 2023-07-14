b6260ee0e979b066815d8d77a4d526d129b969f5
dominicmwikya <dominicmwikya50@gmail.com>
2023-07-14 18:33:07 +0000

    Update  102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0';
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
 				if (i != 98  || j != 99)
 				{
 					putchar(',');
@@ -27,5 +27,7 @@ int main(void)
 			}
 		}
 	}
+        putchar('\n');
+
 	return (0);
 }
