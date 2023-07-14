f0b530729a1dd248fc3244f839c31b8452ffc954
dominicmwikya <dominicmwikya50@gmail.com>
2023-07-14 16:29:47 +0000

    Update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 { 
-	char lowerCase[26]="abcdefghijklmnopqrstuvwxyz"; 
+	char lowerCase[26] = "abcdefghijklmnopqrstuvwxyz"; 
 	int i;
 	
 	for (i = 0; i < 26; i++)
         {
-   	     putchar(lowerCase[i]);
+		putchar(lowerCase[i]);
        	}
 	putchar('\n');
        	return (0);
