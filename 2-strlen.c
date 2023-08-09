e4bd5094f3a9f9aa0f8c231c08d72a08a491fedf
dominicmwikya <dominicmwikya50@gmail.com>
2023-08-09 04:24:56 -0700

    libraries

---
+++ 2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input stringeturn.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
