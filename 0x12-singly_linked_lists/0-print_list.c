9a170d6f3e4324cc3b6c8dd0e10961b0358d3412
dominicmwikya <dominicmwikya50@gmai.com>
2023-08-25 05:13:48 -0700

    update print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,10 +12,14 @@ size_t print_list(const list_t *h)
 	
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		count++;
 	}
