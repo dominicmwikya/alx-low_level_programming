dee2af2aa59bc33acb01c08f4527e6ca7c9358b5
dominicmwikya <dominicmwikya50@gmai.com>
2023-08-25 04:58:40 -0700

    updateprintlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,24 +6,18 @@
  *
  * Return: total number of the nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-
-	while (h)
+	int count = 0;
+	
+	while (h != NULL)
 	{
-		if (!h->str)
-		{
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		}
 		else
-		{
-			printf("[%u] %s\n", h->len, h->str);
-		}
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
 	return (count);
 }
-
